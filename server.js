c6708a174e21e1ef6386af4199607a9adcb2c006
Leo <leonard.njura@gmail.com>
2019-09-30 11:42:59 +0300

    Heroku deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const { db, DATABASE_NAME } = require('./config/database');
 
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -15,23 +15,42 @@ const { NODE_ENV } = process.env;
 const isDevMode = NODE_ENV === 'development';
 const isProductionMode = NODE_ENV === 'production';
 
+// Connect to postgres
 db.authenticate()
   .then(() => console.log(`\n[${DATABASE_NAME}]Database connected..`))
   .catch(err => console.log(`Error ${err}`));
 
-app.get('/', (req, res) => {
+// Use routes
+app.get('/api', (req, res) => {
   res.json({
-    msg: 'welcome to personnel tasks api'
+    msg: 'welcome to customer care api'
   });
 });
 
-// use routes
 app.use('/api/auth', require('./routes/api/personnel'));
 app.use('/api/tasks', require('./routes/api/tasks'));
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
-  console.log(` \nPersonnel server started on port ${PORT}`);
+// Hitting / will serve static assets if production
+// remember to insert build script, :)
+if (isProductionMode) {
+  // set static folder
+  app.use(express.static('build'));
+
+  //serve index.html for any req
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
+// Hitting / will serve api if development
+app.get('/', (req, res) => {
+  res.json({ msg: 'welcome to customer care api' });
 });
 
-module.exports = { app, server };
+const PORT = process.env.PORT || 5000;
+
+const mode = isDevMode ? 'DevMode' : 'ProductionMode';
+app.listen(PORT, () => {
+  console.log({ mode });
+  console.log(`🥒️ Customer care server started on port ${PORT}..`);
+});
